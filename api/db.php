89d7deccc3a8b0abe0e66f1392cebf85e070deeb
anand.surendran <anand.surendran@aline-consulting.com>
2017-12-26 22:35:22 +0530

    changed username & password for beta

--- api/db.php
+++ api/db.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost","root","","madurai_giants_db");
+$con=mysqli_connect("localhost","maduraigiants","maduraigiants","madurai_giants_db");
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
